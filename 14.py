30427de20eee027a652263d78a6c235b1c91b7d4
giralaw <al.skeldon@gmail.com>
2024-12-15 15:42:34 -0500

    added entropy method to day 14

--- 14.py
+++ 14.py
@@ -15,6 +15,7 @@ from collections import defaultdict, deque
 import functools
 from aoc_tools import *
 from statistics import mode, multimode
+import matplotlib.pyplot as plt
 sys.setrecursionlimit(10**6)
 
 infile = sys.argv[1] if len(sys.argv)>=2 else '14.in'
@@ -31,11 +32,18 @@ qs = [0,0,0,0]
 
 G = [['.' for _ in range(C)] for _ in range(R)]
 
+
+
+pos = np.empty((1,4), dtype = int)
+
 for line in S.split('\n'):
     # locations after 100 seconds
     # number of robots in each quadrant, multiply each occ together
     # ignore robots on the line
     px,py,vx,vy = nums(line)
+    new_row = np.array([px,py,vx,vy])
+    pos = np.vstack([pos, new_row])
+
     fin = ((100*vx+px) % C, (100*vy +py) % R)
 
     if fin[0] < C//2 and fin[1] < R//2:
@@ -51,6 +59,10 @@ p1 = 1
 for a in qs:
     p1 *= a
 
+
+vel = pos[1:,2:]
+pos = pos[1:,:2]
+
 # cycle starts at 10430 apparently :/
 #26 seconds to use deepcopy to generate new G
 # 25 for gen from scratch, so marginally faster, not too significant
@@ -59,6 +71,16 @@ for a in qs:
 start = time.time()
 #pics = defaultdict()
 
+# entropy function for numpy approach
+def calc_entropy(pos, w,h):
+    grid = np.zeros((h,w), dtype=int)
+    np.add.at(grid, (pos[:,1], pos[:,0]), 1)
+    counts = np.bincount(grid.flatten())
+    probs = counts[counts >0] / counts.sum()
+    entropy = -np.sum(probs* np.log2(probs))
+    return entropy
+
+entropies = []
 for t in range(0,10500):
     pic = [['.' for c in range(C)] for r in range(R)]
     for line in S.split('\n'):
@@ -67,6 +89,11 @@ for t in range(0,10500):
         pic[fin[1]][fin[0]] = '#'
     if t % 1000 == 0:
         print(t)
+    
+    # alternate approach -- calc entropy, use numpy and mpl
+    entropies.append(calc_entropy(pos,C,R))
+    pos =(pos+vel) % [C,R]
+    
     # this section was to find the cycle, though it wasn't
     # really integral to solving p2
     #if pic in pics.values():
@@ -87,8 +114,22 @@ for t in range(0,10500):
         gstr.append(''.join(row))
     if found == True:
         print('\n'.join(gstr))
-        break
+        #break
     #time.sleep(0.1)
+
+# entropy and matplotlib fun stuff
+min_ent = min(entropies)
+sol = entropies.index(min_ent)
+print(f'min entropy {min_ent} is at {sol}')
+indices = list(range(len(entropies)))
+plt.plot(indices, entropies)
+plt.text(sol,entropies[sol], f'({sol}, {entropies[sol]})',fontsize=8, ha='right')
+plt.xlabel('Time')
+plt.ylabel('entropy')
+plt.title('Plot of entropy vs. time')
+plt.show()
+
+
 fin = time.time() - start
 print('time for part 2 is',fin)
 
